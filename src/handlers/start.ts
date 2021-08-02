a9f8744ac4a791fafdd352fa2b06af6f92ab979e
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 17:09:33 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,12 +2,12 @@ import { Composer, Markup } from 'telegraf';
 
 export const Start = Composer.command('start', async (ctx) => {
     let text =
-        `Hi <a href="tg://user?id=${ctx.from.id}"></a>\n` +
-        `I Play Songs in Group Voice Chats.\n` +
-        `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
+        `Hi I'm LOVELY 2.0 <a href="https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">🌸</a>` +
+        `I Play Songs in Group Voice Chats very smoothly and without any lags add me and my assistant @LOVELY2VC in your group for enjoying music.\n` +
+        `I'm developed by <a href="t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
-            [Markup.button.url('View Source', 'https://github.com/ArnabXD/TGVCBot')]
+            [Markup.button.url('𝗦𝗨𝗣𝗣𝗢𝗥𝗧 𝗚𝗥𝗢𝗨𝗣', 'https://t.me/LOVELYAPPEAL')]
         ]),
         disable_web_page_preview: true
     })
