3184554d24eddb6ea4b1f01f1d21547da67528f3
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 09:55:03 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,14 +2,14 @@ import { Composer, Markup } from 'telegraf';
 
 export const Start = Composer.command('start', async (ctx) => {
     let text =
-        `нєℓℓο <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
-        'ι'м [,](https://telegra.ph/file/38f9badec49fd74a2ba93.jpg) ℓονєℓγ 2.0'
-'ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.'
-'∂єνєℓορє∂ ϐγ τєαм [ℓονєℓγ](https://t.me/ABOUTVEDMAT) .'
-'τнαиκѕ το [Λгɳαɓ](t.me/Arnab431) ƒοя нєℓριиg υѕ ƒοя ϲяєατιиg ѕυρєя ϐοτ.'
+        `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
+        `ι'м <a href=a"https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">ℓονєℓγ 2.0 .\n` +
+        ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.
+∂єνєℓορє∂ ϐγ τєαм <a href="https://t.me/ABOUTVEDMAT">𝗟𝗢𝗩𝗘𝗟𝗬</a>`
+        `τнαиκѕ το <a href="http://t.me/Arnab431">Λгɳαɓ<a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
-            [Markup.button.url('➕𝘼𝘿𝘿 𝙏𝙊 𝙂𝙍𝙊𝙐𝙋➕', 't.me/LOVELY2_ROBOT?startgroup=true')]
+            [Markup.button.url('➕𝗔𝗗𝗗 𝗧𝗢 𝗚𝗥𝗢𝗨𝗣➕', 'https://t.me/LOVELY2_ROBOT?startgroup=true')]
         ]),
         disable_web_page_preview: true
     })
