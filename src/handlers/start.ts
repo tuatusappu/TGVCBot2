87aa95426629e895d80e42404a04f13024a35771
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 17:13:02 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -2,7 +2,7 @@ import { Composer, Markup } from 'telegraf';
 
 export const Start = Composer.command('start', async (ctx) => {
     let text =
-        `Hi I'm LOVELY 2.0 <a href="https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">🌸</a>` +
+        `Hi I'm LOVELY 2.0 <a href="https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">🌸</a>\n` +
         `I Play Songs in Group Voice Chats very smoothly and without any lags add me and my assistant @LOVELY2VC in your group for enjoying music.\n` +
         `I'm developed by <a href="t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a`
     await ctx.replyWithHTML(text, {
