fd06985c0123817d37fe6e5c22da45c38a9bd92e
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 09:57:11 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -4,8 +4,8 @@ export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
         `ι'м <a href=a"https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">ℓονєℓγ 2.0 .\n` +
-        ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.
-∂єνєℓορє∂ ϐγ τєαм <a href="https://t.me/ABOUTVEDMAT">𝗟𝗢𝗩𝗘𝗟𝗬</a>`
+        `ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.\n` +
+        `∂єνєℓορє∂ ϐγ τєαм <a href="https://t.me/ABOUTVEDMAT">𝗟𝗢𝗩𝗘𝗟𝗬</a>`
         `τнαиκѕ το <a href="http://t.me/Arnab431">Λгɳαɓ<a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
