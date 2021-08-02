ebdd9bbdcd316826cd266cc5dac512107c6dca15
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 09:37:40 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -3,10 +3,10 @@ import { Composer, Markup } from 'telegraf';
 export const Start = Composer.command('start', async (ctx) => {
     let text =
         `нєℓℓο <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
-        'ι'м [,](https://telegra.ph/file/38f9badec49fd74a2ba93.jpg) ℓονєℓγ 2.0
-ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.
-∂єνєℓορє∂ ϐγ τєαм [ℓονєℓγ](https://t.me/ABOUTVEDMAT) .
-τнαиκѕ το [Λгɳαɓ](t.me/Arnab431) ƒοя нєℓριиg υѕ ƒοя ϲяєατιиg ѕυρєя ϐοτ.'
+        'ι'м [,](https://telegra.ph/file/38f9badec49fd74a2ba93.jpg) ℓονєℓγ 2.0'
+'ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.'
+'∂єνєℓορє∂ ϐγ τєαм [ℓονєℓγ](https://t.me/ABOUTVEDMAT) .'
+'τнαиκѕ το [Λгɳαɓ](t.me/Arnab431) ƒοя нєℓριиg υѕ ƒοя ϲяєατιиg ѕυρєя ϐοτ.'
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('➕𝘼𝘿𝘿 𝙏𝙊 𝙂𝙍𝙊𝙐𝙋➕', 't.me/LOVELY2_ROBOT?startgroup=true')]
