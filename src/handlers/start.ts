fe08a50440f7a5b5066f1ad5e6d10ce98627cee8
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 10:00:32 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -6,7 +6,7 @@ export const Start = Composer.command('start', async (ctx) => {
         `ι'м <a href=a"https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">ℓονєℓγ 2.0 .\n` +
         `ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.\n` +
         `∂єνєℓορє∂ ϐγ τєαм <a href="https://t.me/ABOUTVEDMAT">𝗟𝗢𝗩𝗘𝗟𝗬</a>`
-        `τнαиκѕ το <a href="http://t.me/Arnab431">Λгɳαɓ<a>`
+        `τнαиκѕ το <a href="http://t.me/Arnab431">Λгɳαɓ<a> ƒοя нєℓριиg υѕ ƒοя ϲяєατιиg ѕυρєя ϐοτ.`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('➕𝗔𝗗𝗗 𝗧𝗢 𝗚𝗥𝗢𝗨𝗣➕', 'https://t.me/LOVELY2_ROBOT?startgroup=true')]
