b777e85bcdf539f54a474f63ad10382de454afa1
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-02 10:20:37 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -3,13 +3,11 @@ import { Composer, Markup } from 'telegraf';
 export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.first_name}</a>\n` +
-        `ι'м <a href=a"https://telegra.ph/file/38f9badec49fd74a2ba93.jpg">ℓονєℓγ 2.0 .\n` +
-        `ι ϲαи ρℓαγ мυѕιϲ ιи γουя gяουρ νοιϲє ϲнατ νєяγ ѕмοοτнℓγ αи∂ ωιτнουτ αиγ ℓαgѕ.\n` +
-        `∂єνєℓορє∂ ϐγ τєαм <a href="https://t.me/ABOUTVEDMAT">𝗟𝗢𝗩𝗘𝗟𝗬</a>.\n` +
-        `τнαиκѕ το <a href="http://t.me/Arnab431">Λгɳαɓ<a> ƒοя нєℓριиg υѕ ƒοя ϲяєατιиg ѕυρєя ϐοτ.`
+        `I Play Songs in Group Voice Chats.\n` +
+        `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
-            [Markup.button.url('➕𝗔𝗗𝗗 𝗧𝗢 𝗚𝗥𝗢𝗨𝗣➕', 'https://t.me/LOVELY2_ROBOT?startgroup=true')]
+            [Markup.button.url('View Source', 'https://github.com/ArnabXD/TGVCBot')]
         ]),
         disable_web_page_preview: true
     })
