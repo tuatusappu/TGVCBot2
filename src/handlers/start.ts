37c6d0ddd2438605723e5fcadefefacb51607e2d
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-03 13:03:17 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -3,8 +3,8 @@ import { Composer, Markup } from 'telegraf';
 export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name}</a>\n` +
-        `I Play Songs in Group Voice Chats.\n` +
-        `I['](https://telegra.ph/file/cbbf01ed6f013ce07d54f.jpg)m developed by <a href="https://t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a>`
+        `I Play Songs in Group Voice Chats add me and my assistant @LOVELY2VC in your group to enjoying music.\n` +
+        `I'm developed by <a href="https://t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('➕𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟➕', 'https://t.me/ABOUTVEDMAT')]
