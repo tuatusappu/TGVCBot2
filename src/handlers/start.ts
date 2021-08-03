051ce1a574df029328fb5cd4cd6248583158655b
tuatusappu <71092937+tuatusappu@users.noreply.github.com>
2021-08-03 12:59:36 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -4,7 +4,7 @@ export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name}</a>\n` +
         `I Play Songs in Group Voice Chats.\n` +
-        `I <a href="https://telegra.ph/file/cbbf01ed6f013ce07d54f.jpg">'</a>m developed by <a href="https://t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a>`
+        `I['](https://telegra.ph/file/cbbf01ed6f013ce07d54f.jpg)m developed by <a href="https://t.me/TUSHAR204">「ƬƲֆӇƛƦ ✘ ԼƠꪜЄԼƳ」🇮🇳</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('➕𝗨𝗣𝗗𝗔𝗧𝗘𝗦 𝗖𝗛𝗔𝗡𝗡𝗘𝗟➕', 'https://t.me/ABOUTVEDMAT')]
